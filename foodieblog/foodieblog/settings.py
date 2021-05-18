3e36622cb0856ad7a3b38f66a5df12abacda0347
Sarahanyan <saranyango@gmail.com>
2021-05-18 22:32:13 +0300

    Added a html/css/js template and refactored appropriately. Added static files handling for the css js and images

--- foodieblog/foodieblog/settings.py
+++ foodieblog/foodieblog/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'posts',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'foodieblog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,6 +120,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+
+STATIC_ROOT = Path(BASE_DIR).parent / 'static_root'
+MEDIA_ROOT = Path(BASE_DIR).parent / 'media_root'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- foodieblog/posts/views.py
+++ foodieblog/posts/views.py
@@ -1,3 +1,28 @@
+from django.conf import settings
+from django.http import HttpResponse
 from django.shortcuts import render
+from pathlib import Path
 
 # Create your views here.
+
+
+def home_view(request):
+    print(settings.BASE_DIR)
+    print(Path(settings.BASE_DIR) / 'staticfiles')
+    print(Path(__file__).resolve())
+    directory = Path(settings.BASE_DIR).parent
+    template = "index.html"
+    context = {
+        "directo": Path(settings.BASE_DIR / "staticfiles")
+    }
+    return render(request, template, context)
+
+
+def about_view(request):
+    template = "about.html"
+    return render(request, template, context)
+
+
+def contact_view(request):
+    template = "contact.html"
+    return render(request, template, context)
